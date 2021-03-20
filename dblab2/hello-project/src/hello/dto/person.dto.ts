d8dc486b55877cc456f2b2fcb81564ae737994c7
hoomch <hoomonchamani@yahoo.com>
2021-03-20 11:23:55 +0330

    lab2 intro done

--- dblab2/hello-project/src/hello/dto/person.dto.ts
+++ dblab2/hello-project/src/hello/dto/person.dto.ts
@@ -1,11 +1,14 @@
 import {Length, IsOptional, Min, IsNumber} from 'class-validator';
+import { ApiProperty, ApiPropertyOptional }  from '@nestjs/swagger';
 
 export class PersonDto {
     @Length(3,10)
+    @ApiProperty({ description: 'Enter your name > ', minLength: 3, default: `Ali`, maxLength: 10})
     name: string;
 
     @IsNumber()
     @IsOptional()
     @Min(1960)
+    @ApiPropertyOptional({ description: 'Optional', default:1988, minimum: 1960})
     year: number;
 }
--- dblab2/hello-project/src/hello/hello.controller.ts
+++ dblab2/hello-project/src/hello/hello.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body, Get, Header, Query } from '@nestjs/common';
 import { HelloService } from './hello.service';
 import { PersonDto } from './dto/person.dto';
 import { INSTANCE_METADATA_SYMBOL } from '@nestjs/core/injector/instance-wrapper';
+import { ApiResponse, ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
 
 @Controller('hello')
 export class HelloController {
@@ -9,12 +10,29 @@ export class HelloController {
     constructor(
         private readonly helloService: HelloService,
     ) {}
+
+    @Header('Content-Type', 'application/json')
+    @ApiResponse({ status: 200, description: 'Say Hello!!!' })
+
     @Post('welcome')
     @Header('Content-Type', 'application/json')
     async sayWelcome(@Body() PersonDto: PersonDto): Promise<{data : String}> {
         let msg = await this.helloService.welcome(PersonDto);
         return {data : msg};
     }
+
+    @ApiResponse({ status: 200 })
+    @ApiQuery({
+        name: 'name',
+        required: true,
+        type: String,
+    })
+    @ApiQuery({
+        name: 'year',
+        required: false,
+        type: Number,
+        description: `you can igonre this`
+    })
 
     @Get('welcome')
     async sayWelcome2(@Query('name') iName, @Query('year') iYear): Promise<{data : String}> {

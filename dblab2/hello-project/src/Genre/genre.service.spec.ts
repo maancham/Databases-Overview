f6fe437f67530168fa000b1f65ff0f015d197740
hoomch <hoomonchamani@yahoo.com>
2021-03-30 23:45:09 +0430

    part 3 partially done - dbeaver remaining, swagger done

---
+++ dblab2/hello-project/src/Genre/genre.service.spec.ts
@@ -0,0 +1,18 @@
+import  GenreService  from './genre.service';
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('GenreService', () => {
+  let service: GenreService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [GenreService],
+    }).compile();
+
+    service = module.get<GenreService>(GenreService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
